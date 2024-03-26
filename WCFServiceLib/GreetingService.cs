9aa37653c258e4b566d713b222222eecf48bfb15
Ashish Panchal <asis.dynamic.methods@gmail.com>
2024-03-26 13:11:56 +0530

    Create Service Contract as GetGretting

---
+++ WCFServiceLib/GreetingService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFServiceLib
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class GreetingService : IGreetingService
+    {
+        public string GetGreeting(string name)
+        {
+            return $"Hello, {name}! Welcome to the WCF named pipe example.";
+        }
+    }
+}
