7d3edcf1bccdac03da780a4c05ca571e1105f546
Ashish Panchal <asis.dynamic.methods@gmail.com>
2024-03-28 11:46:13 +0530

    Create Service Project to Start WCF Service Also manage Start Service as Debug mode

---
+++ HostService/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostService
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+#if DEBUG
+            //If the mode is in debugging
+            //create a new service instance
+            ServiceHello myService = new ServiceHello();
+            //call the start method - this will start the Timer.
+            myService.DebugStart();
+            //Set the Thread to sleep
+            //Thread.Sleep(60000);
+            //Call the Stop method-this will stop the Timer.
+            //myService.Stop();
+#else
+            //The following is the default code - You may fine tune
+            //the code to create one instance of the service on the top
+            //and use the instance variable in both debug and release mode
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new ServiceHello()
+            };
+            ServiceBase.Run(ServicesToRun);
+#endif
+
+        }
+    }
+}
---
+++ HostService/ServiceHello.cs
@@ -0,0 +1,63 @@
+﻿using HelloService;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostService
+{
+    public partial class ServiceHello : ServiceBase
+    {
+        public ServiceHello()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            StartWcfHelloService();
+            
+        }
+
+        private void StartWcfHelloService()
+        {
+            try
+            {
+                //Uri baseAddress = new Uri(uriString);
+                ServiceHost serviceHost = new ServiceHost(typeof(HelloService.HelloService));
+                Console.WriteLine("Welcome to Hello Service  Loading....");
+                serviceHost.Open();
+                Console.WriteLine("Hello Service is Started...");
+                Console.ReadKey();
+            }
+            catch (UriFormatException ex)
+            {
+                Console.WriteLine("Error creating URI: " + ex.Message);
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception:" + ex.ToString());
+                Console.ReadKey();
+
+            }
+        }
+
+        protected override void OnStop()
+        {
+        }
+
+
+
+        internal void DebugStart()
+        {
+            StartWcfHelloService();
+        }
+    }
+}
